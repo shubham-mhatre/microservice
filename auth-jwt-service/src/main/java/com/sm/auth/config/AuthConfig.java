0ce0ba992044174b79f6630513882555ad718607
shubham <shubhammahtre@bdo.in>
2024-08-17 14:08:14 +0530

    enable spring security

--- auth-jwt-service/src/main/java/com/sm/auth/config/AuthConfig.java
+++ auth-jwt-service/src/main/java/com/sm/auth/config/AuthConfig.java
@@ -2,12 +2,27 @@ package com.sm.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class AuthConfig {
 
+	 @Bean
+	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	        http
+	            .authorizeHttpRequests(authorize -> authorize
+	                .requestMatchers("/auth/register", "/auth/token", "/auth/validate").permitAll()
+	                .anyRequest().authenticated()
+	            )
+	            .csrf().disable();
+	        
+	        return http.build();
+	    }
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
