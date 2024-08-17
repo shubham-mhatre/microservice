16e1761f5e27a951e858c06be5b9c46e723c527d
shubham <shubhammahtre@bdo.in>
2024-08-17 12:56:29 +0530

    auth service initial setup

---
+++ auth-jwt-service/src/main/java/com/sm/auth/AuthJwtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sm.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthJwtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthJwtServiceApplication.class, args);
+	}
+
+}
